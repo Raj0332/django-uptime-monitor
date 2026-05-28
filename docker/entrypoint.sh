096164632a3c64513fa3c9ff21c7b67cde5f37eb
raj <rajkm332@gmail.com>
2026-05-28 15:55:37 +0530

    dockerfile fixes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
-.#!/bin/sh
+#!/bin/sh
 set -e
 
 case "$1" in
@@ -6,8 +6,10 @@ case "$1" in
     echo "Running migrations..."
     python manage.py makemigrations monitor --noinput
     python manage.py migrate --noinput
+
     echo "Collecting static files..."
     python manage.py collectstatic --noinput
+
     echo "Starting Gunicorn..."
     exec gunicorn uptime.wsgi:application \
       --bind 0.0.0.0:8000 \
@@ -18,19 +20,17 @@ case "$1" in
       --error-logfile - \
       --log-level info
     ;;
+
   worker)
-    echo "Starting Celery worker..."
-    exec celery -A uptime worker \
-      --loglevel=info \
-      --concurrency=4 \
-      -E
+    exec celery -A uptime worker --loglevel=info --concurrency=4 -E
     ;;
+
   beat)
-    echo "Starting Celery beat..."
     exec celery -A uptime beat \
       --loglevel=info \
       --scheduler django_celery_beat.schedulers:DatabaseScheduler
     ;;
+
   *)
     exec "$@"
     ;;
