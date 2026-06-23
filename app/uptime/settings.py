8fcff14d67f4464bf1ede80278a9dca03f09aef9
raj <rajkm332@gmail.com>
2026-06-24 01:29:41 +0530

    changes in allowed hosts

--- app/uptime/settings.py
+++ app/uptime/settings.py
@@ -12,7 +12,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
     DEBUG=(bool, False),
-    ALLOWED_HOSTS = ["*"],
     TIME_ZONE=(str, "UTC"),
     PROMETHEUS_MULTIPROC_DIR=(str, "/tmp/prometheus_multiproc"),
 )
